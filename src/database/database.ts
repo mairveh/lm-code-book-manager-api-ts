aef82d8788a23f446332d6acb5dc6ecfb0b71532
Merve <babutine@yahoo.com>
2023-11-26 14:23:39 +0000

    refactor: load database variables from config file through .env

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,21 @@
-import { Sequelize } from "sequelize";
+import { Sequelize, Dialect } from "sequelize";
+import { APP_CONFIG } from "../../config";
 
 // TODO: This should be external config
-export const sequelize = new Sequelize("sqlite::memory:");
+export let sequelize : Sequelize;
+//export let sequelize = new Sequelize("postgres://bookshopuser:super-secret-password@127.0.0.1:5432/bookshop");
+
+if (APP_CONFIG.nodeEnv !== "dev") {
+	sequelize = new Sequelize(
+		APP_CONFIG.dbName,
+		APP_CONFIG.dbUserName,
+		APP_CONFIG.dbPassword,
+		{
+			host: APP_CONFIG.dbHost,
+			port: parseInt(APP_CONFIG.dbPort as string),
+			dialect: "postgres",
+		}
+	);
+} else {
+	sequelize = new Sequelize("sqlite::memory:");
+}
