1785146e506b021726d896a33ee23a13efc9d520
Merve <babutine@yahoo.com>
2023-11-27 18:38:11 +0000

    Revert "refactor: load database variables from config file through .env"
    This reverts commit aef82d8788a23f446332d6acb5dc6ecfb0b71532.
    
    undo commit for database.ts

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,21 +1,4 @@
-import { Sequelize, Dialect } from "sequelize";
-import { APP_CONFIG } from "../../config";
+import { Sequelize } from "sequelize";
 
 // TODO: This should be external config
-export let sequelize : Sequelize;
-//export let sequelize = new Sequelize("postgres://bookshopuser:super-secret-password@127.0.0.1:5432/bookshop");
-
-if (APP_CONFIG.nodeEnv !== "dev") {
-	sequelize = new Sequelize(
-		APP_CONFIG.dbName,
-		APP_CONFIG.dbUserName,
-		APP_CONFIG.dbPassword,
-		{
-			host: APP_CONFIG.dbHost,
-			port: parseInt(APP_CONFIG.dbPort as string),
-			dialect: "postgres",
-		}
-	);
-} else {
-	sequelize = new Sequelize("sqlite::memory:");
-}
+export const sequelize = new Sequelize("sqlite::memory:");
